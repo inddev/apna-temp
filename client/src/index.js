65252a265a3954931d81e7284194e56363f213e1
Anilkokkul <anilkokkul1997@gmail.com>
2024-08-01 15:47:59 +0530

    "Added Panchang component as child route to root path in router configuration"

--- client/src/index.js
+++ client/src/index.js
@@ -4,11 +4,19 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Panchang from "./components/Panchang";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Panchang />,
+      },
+      
+    ],
   },
 ]);
 
