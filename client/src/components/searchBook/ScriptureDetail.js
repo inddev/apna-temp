765e21edbac7237eff018840084ffe818e5b6c92
Anilkokkul <anilkokkul1997@gmail.com>
2024-08-02 16:30:29 +0530

    "Replace Workspace component with div element in ScriptureDetail.js"

--- client/src/components/searchBook/ScriptureDetail.js
+++ client/src/components/searchBook/ScriptureDetail.js
@@ -1,4 +1,3 @@
-import Workspace from "./Workspace";
 import VerseCard from "./VerseCard";
 import { useState, useEffect, useCallback } from "react";
 import axios from "axios";
@@ -23,48 +22,46 @@ const ScriptureDetail = () => {
 
   const chapters = Array.from({ length: 18 }, (_, i) => i + 1);
   return (
-    <Workspace>
-      <div>
-        <div className=" flex items-center justify-center gap-5 p-4 shadow-lg">
-          <h1 className=" text-xl font-bold">Shrimad Bhagavat Gita</h1>
-          <select
-            value={selectedChapter}
-            onChange={(e) => {
-              setSelectedChapter(parseInt(e.target.value));
-              setSelectedVerse(1);
-            }}
-            className="border p-3 rounded-lg"
-          >
-            {chapters.map((chapter) => (
-              <option key={chapter} value={chapter}>
-                Chapter {chapter}
-              </option>
-            ))}
-          </select>
-          <select
-            value={selectedVerse}
-            onChange={(e) => {
-              setSelectedVerse(parseInt(e.target.value));
-            }}
-            className="border p-3 rounded-lg"
-          >
-            {data.map((verse) => (
-              <option key={verse.verse} value={verse.verse}>
-                Verse {verse.verse}
-              </option>
-            ))}
-          </select>
-        </div>
-        <div className="w-[80%]  mx-auto mt-10 flex justify-center items-baseline">
-          {selectedVerseData && (
-            <VerseCard
-              text={selectedVerseData.text}
-              translations={selectedVerseData.translations}
-            />
-          )}
-        </div>
+    <div>
+      <div className=" flex items-center justify-center gap-5 p-4 shadow-lg">
+        <h1 className=" text-xl font-bold">Shrimad Bhagavat Gita</h1>
+        <select
+          value={selectedChapter}
+          onChange={(e) => {
+            setSelectedChapter(parseInt(e.target.value));
+            setSelectedVerse(1);
+          }}
+          className="border p-3 rounded-lg"
+        >
+          {chapters.map((chapter) => (
+            <option key={chapter} value={chapter}>
+              Chapter {chapter}
+            </option>
+          ))}
+        </select>
+        <select
+          value={selectedVerse}
+          onChange={(e) => {
+            setSelectedVerse(parseInt(e.target.value));
+          }}
+          className="border p-3 rounded-lg"
+        >
+          {data.map((verse) => (
+            <option key={verse.verse} value={verse.verse}>
+              Verse {verse.verse}
+            </option>
+          ))}
+        </select>
       </div>
-    </Workspace>
+      <div className="w-[80%]  mx-auto mt-10 flex justify-center items-baseline">
+        {selectedVerseData && (
+          <VerseCard
+            text={selectedVerseData.text}
+            translations={selectedVerseData.translations}
+          />
+        )}
+      </div>
+    </div>
   );
 };
 
